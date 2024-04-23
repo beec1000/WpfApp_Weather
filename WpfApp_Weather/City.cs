e827ed92166ad95921f17957a5094d3822abfb11
beec1000 <beec1000.minecraft@gmail.com>
2024-04-23 13:16:07 +0200

    Add project files.

---
+++ WpfApp_Weather/City.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_Weather
+{
+    internal class City
+    {
+        public string Name { get; set; }
+        public float Temperature { get; set; }
+        public float Humidity { get; set; }
+        public float WindStrength { get; set; }
+    }
+}
---
+++ WpfApp_Weather/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_Weather
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<City> cities = new List<City>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            List.Items.Add(Name.Text + "|" + Temperature.Text + "°C" + "|" + Humidity.Text + "%" + "|" + Wind.Text + "km/h");
+            Name.Text = string.Empty;
+            Temperature.Text = string.Empty;
+            Humidity.Text = string.Empty;
+            Wind.Text = string.Empty;
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = List.SelectedItems;
+
+        }
+    }
+}
