a701fea418b17d77c94222bfa5938cb0f2b1ef3e
beec1000 <beec1000.minecraft@gmail.com>
2024-05-14 09:43:52 +0200

    u

--- WpfApp_Weather/City.cs
+++ WpfApp_Weather/City.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,9 +14,42 @@ namespace WpfApp_Weather
         public float Humidity { get; set; }
         public float WindStrength { get; set; }
 
+        public City(string name, float temperature, float humidity, float windStrength)
+        {
+            Name = name;
+            Temperature = temperature;
+            Humidity = humidity;
+            WindStrength = windStrength;
+        }
+
+        public static List<City> FromFile(string filePath)
+        {
+            List<City> cities = new List<City>();
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string[] p = line.Split(' ');
+
+                if (p.Length == 4 &&
+                    float.TryParse(p[1], out float temperature) &&
+                    float.TryParse(p[2], out float humidity) &&
+                    float.TryParse(p[3], out float wind))
+                {
+                    string cityName = p[0];
+                    City city = new City(cityName, temperature, humidity, wind);
+                    cities.Add(city);
+                }
+            }
+            return cities;
+        }
+
+
         public override string ToString()
         {
-            return $"{Name}"/* | {Temperature}°C | {Humidity}% | {WindStrength}km/h"*/;
+            return $"{Name}";
         }
     }
     
--- WpfApp_Weather/MainWindow.xaml.cs
+++ WpfApp_Weather/MainWindow.xaml.cs
@@ -11,13 +11,12 @@ namespace WpfApp_Weather
         public MainWindow()
         {
             InitializeComponent();
-            cities = new List<City>
-            {
-                new City() { Name = "TesztVaros", Temperature = 69f, Humidity = 420f, WindStrength = 42.069f }
-            };
+            cities = City.FromFile(@"..\..\..\src\cities.txt");
 
             List.ItemsSource = cities;
             List.SelectionChanged += List_SelectionChanged;
+
+            List.SelectedItem = cities[0];
         }
 
         private void List_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -39,27 +38,30 @@ namespace WpfApp_Weather
                 float.TryParse(Humidity.Text, out float humidity) &&
                 float.TryParse(Wind.Text, out float wind))
             {
-                cities.Add(new City()
+                if (!string.IsNullOrWhiteSpace(Name.Text))
                 {
-                    Name = Name.Text,
-                    Temperature = temperature,
-                    Humidity = humidity,
-                    WindStrength = wind
-                });
+                    City newCity = new City(Name.Text, temperature, humidity, wind);
+                    cities.Add(newCity);
 
-                List.ItemsSource = null;
-                List.ItemsSource = cities;
+                    List.ItemsSource = null;
+                    List.ItemsSource = cities;
 
-                Name.Text = string.Empty;
-                Temperature.Text = string.Empty;
-                Humidity.Text = string.Empty;
-                Wind.Text = string.Empty;
+                    Name.Text = string.Empty;
+                    Temperature.Text = string.Empty;
+                    Humidity.Text = string.Empty;
+                    Wind.Text = string.Empty;
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a valid city name.", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
             }
             else
             {
                 MessageBox.Show("Please enter valid values for temperature, humidity, and wind.", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
+
 
         private void DeleteButton_Click(object sender, RoutedEventArgs e)
         {
@@ -76,6 +78,8 @@ namespace WpfApp_Weather
             {
                 cities.Remove(city);
             }
+
+            MessageBox.Show("The selected city has been deleted.", "INFORMATION", MessageBoxButton.OK, MessageBoxImage.Exclamation);
 
             List.ItemsSource = null;
             List.ItemsSource = cities;
